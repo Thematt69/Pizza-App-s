8155be320c6ab7513ae1258e3e1281efab10b5cb
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2022-03-11 21:29:16 +0100

    move firebase config

---
+++ web/firebase-config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDwfUiF4-MRr363UGnSFnK0Bv3938WdLHI",
+  authDomain: "pizza-app-s.firebaseapp.com",
+  projectId: "pizza-app-s",
+  storageBucket: "pizza-app-s.appspot.com",
+  messagingSenderId: "151587877700",
+  appId: "1:151587877700:web:32ba520f437ab6ac7ddd11",
+  measurementId: "G-13LWQLRVCB",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
